01a484ac6fa43060d1e111eb8d7da237a031dfd5
ShayHacohen <shayhacohen14@gmail.com>
2023-07-02 19:54:36 +0300

    More work on malloc_3.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
 
 using std::cout;
 using std::cin;
@@ -19,6 +20,37 @@ size_t _size_meta_data();
 void TEST_print_orders();
 void TEST_print_blocks();
 void TEST_several_stuff();
+void sanity_assertion() {
+#ifdef DEBUG
+    bool valid;
+    if (_num_allocated_blocks() == 0 && _num_free_blocks() == 0) {
+        //Pre-initialization, probably. (Will make sure in my use cases.)
+        valid = true;
+    }
+    else {
+        valid = _num_meta_data_bytes() == (_num_allocated_blocks() * _size_meta_data());
+        assert(_num_meta_data_bytes() == (_num_allocated_blocks() * _size_meta_data()));
+
+        valid = valid && (_num_allocated_bytes() <= _num_allocated_blocks() * 128 * 1024);
+        assert(_num_allocated_bytes() <= _num_allocated_blocks() * 128 * 1024);
+
+        valid = valid && (_num_allocated_blocks() >= 32);
+        assert(_num_allocated_blocks() >= 32);
+
+        valid = valid && (_num_free_bytes() <= _num_free_blocks() * 128 * 1024);
+        assert(_num_free_bytes() <= _num_free_blocks() * 128 * 1024);
+
+        valid = valid && (_num_free_blocks() <= _num_allocated_blocks());
+        assert(_num_free_blocks() <= _num_allocated_blocks());
+
+        valid = valid && (_num_allocated_bytes() + _num_meta_data_bytes() == 128 * 1024 * 32);
+        assert(_num_allocated_bytes() + _num_meta_data_bytes() == 128 * 1024 * 32);
+    }
+    cout << "Sanity check "
+        << (valid ? "passed" : "!!!!!!!!!!!!!!!!!!!!!!failed!!!!!!!!!!!!!!!!!!!!!!")
+        << "." << endl;
+#endif
+}
 
 void print_stats(const char* after_func_name="") {
 #ifdef DEBUG
@@ -26,24 +58,32 @@ void print_stats(const char* after_func_name="") {
         << after_func_name << (strlen(after_func_name) > 0 ? ":\n" : "")
         << "Free blocks: " << _num_free_blocks() << endl
         << "Free bytes: " << _num_free_bytes() << endl
-        << "Total allocated blocks: " << _num_allocated_blocks() << endl
+        << "Total allocated free_blocks: " << _num_allocated_blocks() << endl
         << "Total allocated bytes: " << _num_allocated_bytes() << endl
         << "Total bytes of metadata: " << _num_meta_data_bytes() << endl
-        << "Size of single metadata section: " << _size_meta_data() << endl
-        << "* * * * * *\n" << endl;
+        << "Size of single metadata section: " << _size_meta_data() << endl;
+    sanity_assertion();
+    cout << "* * * * * *\n" << endl;
 #endif
 }
 
 int main() {
+    print_stats("Before any function call");
+
     char *str = (char*)smalloc(strlen("hello") + 1), *orig;
     sprintf(str, "hello");
+    print_stats("smalloc");
 
-    cout << "scalloc: " << str << endl;
-    print_stats();
+    sfree(str);
+    print_stats("sfree");
+
+    str = (char*)smalloc(strlen("hello") + 1);
+    print_stats("smalloc");
 
     str = (char*)srealloc(str, strlen("hello") + 1);
+    //sprintf(str, "hello");
     cout << "srealloc size unchanged: " << str << endl;
-    print_stats();
+    print_stats("srealloc");
 
     str = (char*)srealloc(str, strlen("hello") + 10);
     cout << "srealloc size changed: " << str << endl;
@@ -53,7 +93,7 @@ int main() {
     sfree(str);
     print_stats("sfree");
 
-    str = (char*)scalloc(strlen("hello") + 10, sizeof(char));
+    /*str = (char*)scalloc(strlen("hello") + 10, sizeof(char));
     print_stats("scalloc");
     cout << "sfree & then scalloc; got " << (orig == str ? "same" : "different") << " address" << endl;
     if (orig != str) {
@@ -62,6 +102,6 @@ int main() {
 
     //TEST_print_orders();
     TEST_print_blocks();
-    TEST_several_stuff();
+    TEST_several_stuff();*/
     return 0;
 }
--- malloc_3.cpp
+++ malloc_3.cpp
@@ -5,41 +5,148 @@
 #include <iostream>
 #endif
 
-struct MallocMetadata {
-    size_t size;
-    bool is_free;
-    MallocMetadata* next;
-    MallocMetadata* prev;
-    MallocMetadata* buddy;
-};
-
 const size_t BASE_ORDER_SIZE = 128;
 const int MAX_ORDER = 10;
 const unsigned long BLOCK_COUNT = 32;
 
 const int ORDER_COUNT = MAX_ORDER + 1;
 
+struct MallocMetadata {
+    size_t size;
+    bool is_free;
+    MallocMetadata* next;
+    MallocMetadata* prev;
+    MallocMetadata* buddies[ORDER_COUNT];
+};
+
 class BuddyAllocator {
 private:
     int base_order;
-    MallocMetadata* blocks[MAX_ORDER + 1];
+    MallocMetadata* free_blocks[MAX_ORDER + 1];
+    MallocMetadata* used_blocks = nullptr;
     bool initialized = false;
+    int free_block_count = 0;
+    int total_allocated_blocks = 0;
+    size_t allocated_space = 0;
+    size_t free_space = 0;
 
     //Auxiliary & convenience member functions & properties:
-    size_t order_map[MAX_ORDER + 1];  //Just for minor runtime optimization purposes.
+    size_t order_map[ORDER_COUNT];  //Just for minor runtime optimization purposes.
+    int order_from_size(size_t size) {
+        for (int i = 0; i < ORDER_COUNT; ++i) {
+            if (order_map[i] == size) {
+                return i;
+            }
+        }
+        return -1;
+    }
     float kb_to_b(float kb) { return kb / 1024; }
     float b_to_kb(float b) { return b * 1024; }
     MallocMetadata* __aux_getBlockByAddressTraversal(int order, int index) {
-        return (MallocMetadata*)((char*)blocks[order] + order_map[order] * (index));
+        return (MallocMetadata*)((char*)free_blocks[order] + order_map[order] * (index));
+    }
+    void aux_removeFromBlocksList(MallocMetadata* block, MallocMetadata** head=nullptr) {
+        if (!head) {
+            if (block->is_free) {
+                #ifdef DEBUG
+                std::cout << "aux_removeFromBlocksList only supports implicit list head if block is allocated." << std::endl;
+                #endif
+                return;
+            }
+            head = &used_blocks;
+        }
+        #ifdef DEBUG
+        if (*head == nullptr) {
+            std::cout << "Tried to remove block from an empty list!" << std::endl;
+            return;
+        }
+        #endif
+        if (block->prev) {
+            block->prev->next = block->next;
+        }
+        if (block->next) {
+            block->next->prev = block->prev;
+        }
+        if (*head == block) {
+            *head = block->next;
+        }
+        block->next = nullptr;
+        block->prev = nullptr;
+    }
+
+    void aux_addToBlocksList(MallocMetadata** head_ptr, MallocMetadata* block) {
+        if (*head_ptr == nullptr) {
+            *head_ptr = block;
+            return;
+        }
+        else if ((*head_ptr) > block) {
+            block->next = *head_ptr;
+            block->next->prev = block;
+            *head_ptr = block;
+            return;
+        }
+
+        auto head = *head_ptr;
+        MallocMetadata *next = head->next;
+
+        while(head) {
+            if (next == nullptr) {
+                next = block;
+                return;
+            }
+            else if (head->next < block) {
+                block->next = next;
+                next->prev = block;
+                head->next = block;
+                return;
+            }
+            head = head->next;
+        }
+        #ifdef DEBUG
+        std::cout << "ERROR: aux_addToBlocksList failed." << std::endl;
+        #endif
+    }
+
+    void aux_removeFromFreeBlocks(MallocMetadata* block) {
+        int order = order_from_size(block->size);
+        if (order < 0) {
+            #ifdef DEBUG
+            std::cout << "Attempted to remove block with illegal size: " << block->size << "std::endl";
+            #endif
+            return;
+        }
+        aux_removeFromBlocksList(block, &free_blocks[order]);
+    }
+
+    void aux_mergeStep(MallocMetadata** block_ptr) {
+        auto block = *block_ptr;
+        auto buddy = block->buddies[order_from_size(block->size)];
+        if (!(buddy != nullptr && buddy->is_free)) {
+            #ifdef DEBUG
+            std::cout << "Called aux_mergeStep with non-mergeable blocks.";
+            #endif
+            return;
+        }
+        block = block < buddy ? block : buddy;
+        block->size += buddy->size;
+        aux_removeFromBlocksList(block);
+        aux_removeFromBlocksList(buddy);
+        aux_addToBlocksList(&free_blocks[order_from_size(block->size)], block);
+        *block_ptr = block;
+
+        //Statistics changes due to merging:
+        --free_block_count;
+        free_space += sizeof(MallocMetadata);
+        --total_allocated_blocks;
     }
 public:
     BuddyAllocator(int base_order=BASE_ORDER_SIZE)
         : base_order(base_order) {
-        blocks[0] = nullptr;
+        free_blocks[0] = nullptr;
         order_map[0] = base_order;
         for (int i = 1; i < ORDER_COUNT; ++i) {
             order_map[i] = 2 * order_map[i-1];
-            blocks[i] = nullptr;
+            free_blocks[i] = nullptr;
         }
     }
 
@@ -48,31 +155,39 @@ public:
         initialized = true;
 
         #ifdef DEBUG
-            std::cout << "Initializing buddy allocator." << std::endl;
+        std::cout << "Initializing buddy allocator." << std::endl;
         #endif
 
-        blocks[ORDER_COUNT - 1] = (MallocMetadata*)sbrk(BLOCK_COUNT * order_map[MAX_ORDER]);
+        free_blocks[ORDER_COUNT - 1] = (MallocMetadata*)sbrk(BLOCK_COUNT * order_map[MAX_ORDER]);
 
         *__aux_getBlockByAddressTraversal(MAX_ORDER, 0) = { order_map[ORDER_COUNT - 1], true, nullptr, nullptr, nullptr };
-        blocks[ORDER_COUNT - 1]->next = __aux_getBlockByAddressTraversal(MAX_ORDER, 1);
+        free_blocks[ORDER_COUNT - 1]->next = __aux_getBlockByAddressTraversal(MAX_ORDER, 1);
         *__aux_getBlockByAddressTraversal(MAX_ORDER, BLOCK_COUNT - 1) = { order_map[ORDER_COUNT - 1], true, nullptr, nullptr, nullptr };
         __aux_getBlockByAddressTraversal(MAX_ORDER, ORDER_COUNT - 1)->next = __aux_getBlockByAddressTraversal(MAX_ORDER, ORDER_COUNT - 2);
 
         for (int i = 1; i < BLOCK_COUNT - 1; ++i) {
             auto curr = __aux_getBlockByAddressTraversal(MAX_ORDER, i);
-            *curr = { order_map[ORDER_COUNT - 1], true, nullptr, nullptr, nullptr };
+            *curr = { order_map[ORDER_COUNT - 1], true, nullptr, nullptr };
+            for (auto & buddy : curr->buddies) {
+                buddy = nullptr;
+            }
             curr->prev = __aux_getBlockByAddressTraversal(MAX_ORDER, i - 1);
             curr->next = __aux_getBlockByAddressTraversal(MAX_ORDER, i + 1);
         }
+
+        free_block_count = BLOCK_COUNT;
+        free_space = BLOCK_COUNT * (order_map[MAX_ORDER] - sizeof(MallocMetadata));
+        allocated_space = free_space;
+        total_allocated_blocks = BLOCK_COUNT;
     }
 
-    MallocMetadata* getMinimalMatchingBlock(size_t size, bool split=true) {
+    MallocMetadata* getMinimalMatchingFreeBlock(size_t size, bool split=true) {
         for (int i = 0; i < ORDER_COUNT; ++i) {
             if (order_map[i] < size + sizeof(MallocMetadata)) {
                 continue;
             }
 
-            auto curr = blocks[i];
+            auto curr = free_blocks[i];
             while (curr != nullptr) {
                 if (curr->is_free) {
                     return curr;
@@ -84,52 +199,63 @@ public:
     }
 
     //TESTING STUFF:
-    void TEST_print_orders() {
-    #ifdef DEBUG
-        initialize_blocks();
-        for (int i = 0; i < ORDER_COUNT; ++i) {
-            std::cout << "Order #" << i << ": " << order_map[i] << std::endl;
-        }
-    #endif
-    }
-    void TEST_print_blocks() {
-    #ifdef DEBUG
-        initialize_blocks();
-        for (int i = 0; i < ORDER_COUNT; ++i) {
-            std::cout << "@ @ @\nIterating over blocks of order " << i << " (size: " <<
-                order_map[i] << " bytes)." << std::endl;
-            auto list = blocks[i];
-            int j = 0;
-            while (list != nullptr) {
-                std::cout << "Block #" << j++ << ": addr=" << list << ", size=" << list->size
-                    << ", " << (list->is_free ? "" : "not ") << "free, buddy is " << list->buddy
-                    << std::endl;
-                list = list->next;
-            }
-        }
-    #endif
-    }
+    void TEST_print_orders();
+    void TEST_print_blocks();
+    void TEST_minimal_matching_no_split();
 
-    void TEST_minimal_matching_no_split() {
-    #ifdef DEBUG
-        initialize_blocks();
-        size_t test_set[] = {5, 17, 90, 44, 33, 128, 128 * 1023, 128 * 1000, 128 * 1024 - 41, 128 * 1024 - 40, 128 * 1024 - 39, 128 * 1024};
-        for (auto i : test_set) {
-            std::cout << "Minimal matching for " << i << " without splitting: "
-                << getMinimalMatchingBlock(i, false) << std::endl;
-        }
-        std::cout << "(MallocMetadata size is " << sizeof(MallocMetadata) << ".)" << std::endl;
-    #endif
-    }
+    MallocMetadata *findOrAllocateBlock(size_t size);
+    MallocMetadata *allocateBlock(size_t size);
+    void setBlockFree(MallocMetadata *block, bool free_value);
+
+    size_t _num_free_blocks() const;
+    size_t _num_free_bytes() const;
+    size_t _num_allocated_blocks() const;
+    size_t _num_allocated_bytes() const;
+    size_t _num_meta_data_bytes() const;
+    size_t _size_meta_data() const;
 };
 
-MallocMetadata *allocations = nullptr;
-MallocMetadata *last = nullptr;
+void BuddyAllocator::setBlockFree(MallocMetadata *block, bool free_value) {
+    if (free_value == block->is_free) {
+#ifdef DEBUG
+        std::cout << "WARNING: Attempting to free a block that was already freed!" << std::endl;
+#endif
+        return;
+    }
+    if (free_value) {
+        free_space += block->size;
+        ++free_block_count;
 
-int free_blocks = 0;
-int total_allocated_blocks = 0;
-size_t allocated_space = 0;
-size_t free_space = 0;
+        //Remove from used blocks list:
+        aux_removeFromBlocksList(block);
+        aux_addToBlocksList(&free_blocks[order_from_size(block->size)], block);
+
+        //Merge:
+        MallocMetadata* buddy;
+        while ((buddy = block->buddies[order_from_size(block->size)]) != nullptr) {
+            if (!buddy->is_free) {
+                break;
+            }
+            aux_mergeStep(&block);
+        }
+    }
+    else {
+        free_space -= block->size;
+        --free_block_count;
+        aux_removeFromFreeBlocks(block);
+        aux_addToBlocksList(&used_blocks, block);
+        //TODO: split iteratively
+    }
+    block->is_free = free_value;
+}
+
+MallocMetadata *BuddyAllocator::allocateBlock(size_t size) {
+    initialize_blocks();
+
+    auto block = getMinimalMatchingFreeBlock(size, true);
+    setBlockFree(block, false);
+    return block;
+}
 
 auto allocator = BuddyAllocator();
 
@@ -145,69 +271,20 @@ void TEST_several_stuff() {
     allocator.TEST_minimal_matching_no_split();
 }
 
-void __setBlockFree(MallocMetadata *block, bool free_value) {
-    if (free_value == block->is_free) return;
-    if (free_value) {
-        free_space += block->size;
-        ++free_blocks;
-    }
-    else {
-        free_space -= block->size;
-        --free_blocks;
-    }
-    block->is_free = free_value;
-}
+MallocMetadata* BuddyAllocator::findOrAllocateBlock(size_t size) {
+    if (size == 0 || size > 100000000) return nullptr;
 
-MallocMetadata *__findFreeBlock(size_t size) {
-    auto curr = allocations;
-    while (curr != nullptr) {
-        if (curr->is_free && curr->size >= size) {
-            return curr;
-        }
-        curr = curr->next;
-    }
-    return nullptr;
-}
-
-MallocMetadata *__addBlock(size_t size) {
-    MallocMetadata *addr;
-    if ((addr = (MallocMetadata *)sbrk(size + sizeof(MallocMetadata))) == (void*)-1) {
+    auto block = getMinimalMatchingFreeBlock(size, true);
+    if (!block) {
         return nullptr;
     }
 
-    ++total_allocated_blocks;
-    allocated_space += size;
-
-    MallocMetadata metadata {
-        size, false, nullptr, nullptr
-    };
-    *addr = metadata;
-
-    if (last == nullptr) {
-        allocations = last = addr;
-    }
-    else {
-        last->next = addr;
-        addr->prev = last;
-        last = addr;
-    }
-    return addr;
-}
-
-MallocMetadata* __findOrAllocateBlock(size_t size) {
-    if (size == 0 || size > 100000000) return nullptr;
-
-    auto block = __findFreeBlock(size);
-    if (block) {
-        __setBlockFree(block, false);
-        return block;
-    }
-
-    return __addBlock(size);
+    setBlockFree(block, false);
+    return block;
 }
 
 void* smalloc(size_t size) {
-    auto block_ptr = __findOrAllocateBlock(size);
+    auto block_ptr = allocator.allocateBlock(size);
     if (block_ptr != nullptr) {
         block_ptr += 1;
     }
@@ -215,7 +292,7 @@ void* smalloc(size_t size) {
 }
 
 void* scalloc(size_t num, size_t size) {
-    auto addr = __findOrAllocateBlock(num * size);
+    auto addr = allocator.allocateBlock(num * size);
     if (addr == nullptr) {
         return nullptr;
     }
@@ -235,7 +312,7 @@ void sfree(void* p) {
         return;
     }
 
-    __setBlockFree(pointer, true);
+    allocator.setBlockFree(pointer, true);
 }
 
 void *srealloc(void* oldp, size_t size) {
@@ -257,31 +334,100 @@ void *srealloc(void* oldp, size_t size) {
 
     std::memmove(newp, oldp, size);
 
-    __setBlockFree(old_block, true);
+    allocator.setBlockFree(old_block, true);
     return newp;
 }
 
 
 
 //STATISTICS FUNCTIONS:
-size_t _num_free_blocks() {
-    return free_blocks;
+size_t BuddyAllocator::_num_free_blocks() const {
+    return free_block_count;
 }
 
-size_t _num_free_bytes() {
+size_t BuddyAllocator::_num_free_bytes() const {
     return free_space;
 }
 
-size_t _num_allocated_blocks() {
+size_t BuddyAllocator::_num_allocated_blocks() const {
     return total_allocated_blocks;
 }
 
-size_t _num_allocated_bytes() {
+size_t BuddyAllocator::_num_allocated_bytes() const {
     return allocated_space;
 }
 
-size_t _num_meta_data_bytes() {
+size_t BuddyAllocator::_num_meta_data_bytes() const {
     return total_allocated_blocks * sizeof(MallocMetadata);
+}
+
+size_t BuddyAllocator::_size_meta_data() const {
+    return sizeof(MallocMetadata);
+}
+
+
+//TESTING STUFF:
+void BuddyAllocator::TEST_minimal_matching_no_split() {
+#ifdef DEBUG
+    initialize_blocks();
+    size_t test_set[] = {5, 17, 90, 44, 33, 128, 128 * 1023, 128 * 1000, 128 * 1024 - 41, 128 * 1024 - 40, 128 * 1024 - 39, 128 * 1024};
+    for (auto i : test_set) {
+        std::cout << "Minimal matching for " << i << " without splitting: "
+                  << getMinimalMatchingFreeBlock(i, false) << std::endl;
+    }
+    std::cout << "(MallocMetadata size is " << sizeof(MallocMetadata) << ".)" << std::endl;
+#endif
+}
+
+void BuddyAllocator::TEST_print_blocks() {
+#ifdef DEBUG
+    initialize_blocks();
+    for (int i = 0; i < ORDER_COUNT; ++i) {
+        std::cout << "@ @ @\nIterating over free_blocks of order " << i << " (size: " <<
+                  order_map[i] << " bytes)." << std::endl;
+        auto list = free_blocks[i];
+        int j = 0;
+        while (list != nullptr) {
+            std::cout << "Block #" << j++ << ": addr=" << list << ", size=" << list->size
+                      << ", " << (list->is_free ? "" : "not ") << "free.\nBuddies are: ";
+            for (auto buddy : list->buddies) {
+                std::cout << buddy << " ";
+            }
+            std::cout << std::endl;
+            list = list->next;
+        }
+    }
+#endif
+}
+
+void BuddyAllocator::TEST_print_orders() {
+#ifdef DEBUG
+    initialize_blocks();
+    for (int i = 0; i < ORDER_COUNT; ++i) {
+        std::cout << "Order #" << i << ": " << order_map[i] << std::endl;
+    }
+#endif
+}
+
+//STATISTICS FUNCTIONS:
+size_t _num_free_blocks() {
+    return allocator._num_free_blocks();
+}
+
+size_t _num_free_bytes() {
+    return allocator._num_free_bytes();
+}
+
+size_t _num_allocated_blocks() {
+    return allocator._num_allocated_blocks();
+}
+
+size_t _num_allocated_bytes() {
+    return allocator._num_allocated_bytes();
+}
+
+size_t _num_meta_data_bytes() {
+    return allocator._num_meta_data_bytes();
 }
 
 size_t _size_meta_data() {
